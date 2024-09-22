1319cd6de76cae6e296a57e4812a04b3a76f228b
hongrui <1169948065@qq.com>
2024-09-22 23:45:27 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1>欢迎你使用vuact创建项目</h1>
+<h1>你现在的模板是：Vue+TS</h1>
+
+<strong style="font-size:20px">- 下载所有依赖包 npm i</strong>
+
+<strong style="font-size:20px">- 启动项目 npm run</strong>
+
+<strong style="font-size:20px">
+我的Github：https://github.com/hongruizzz2
+</strong>
+"# vue-ts" 
---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div className="App">
+        <p>
+          欢迎使用vuact-cli脚手架创建Vue3+TS
+        </p>
+        <p>
+          Author：洪睿
+        </p>
+        <a
+            href="https://github.com/hongruizzz2"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+          项目Github地址
+        </a>
+      </div>
+</template>
+
+<style scoped>
+.App {
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    height: 100vh;
+    flex-direction: column;
+}
+
+.App p {
+    font-size: 50px;
+    display: inline-block;
+    padding: 10px 20px; /* 根据需要调整 */
+    background-image: linear-gradient(45deg, green 0%, deepskyblue 100%); /* 渐变颜色 */
+    border-radius: 5px; /* 圆角 */
+    color: white; /* 文字颜色 */
+    text-decoration: none; /* 去掉下划线 */
+    border-radius: 5px;
+    text-align: center;
+}
+
+.App a {
+    text-align: center;
+}
+</style>
